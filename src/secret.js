ca0c458aaeb3288a6f44a232d8114ba5c2fc495e
juliadivan <julia.divan@tufts.edu>
2020-12-01 19:55:51 -0500

    putting in info

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "oGR09SuvJLCt4T";     // fill in your info
+var airtable_baseid = "app" + "qlIa5D1WE3FM2Y"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
